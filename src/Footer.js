89bd6166c1aef5e465a79e1066e81b507a140eee
Ashley Taylor <blueberrypea@gmail.com>
2023-05-08 23:21:03 -0400

    Lab 01 changes

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <footer>
+      &copy; 2023; Ashley Taylor
+    </footer>
+    );
+  }
+}
+
+export default Footer;
