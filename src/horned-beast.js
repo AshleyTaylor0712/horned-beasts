7e86e082e940c64c92108e30bbc0654e9edb0dbc
Ashley Taylor <blueberrypea@gmail.com>
2023-05-09 13:32:51 -0400

    added some css

--- src/horned-beast.js
+++ src/horned-beast.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./HornedBeast.css;"
 
 class HornedBeast extends React.Component {
   render(){
     return(
-      <article>
+      <article className="horned-beast">
         <h2>{this.props.title}</h2>
         <img src={this.props.imageUrl} alt={this.props.description}/>
         <p>{this.props.description}</p>
